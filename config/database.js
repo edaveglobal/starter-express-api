4d7cf71a7a3a4c9826f2f2f292e927ab0fe3f78f
unknown <edaveglobal1@gmail.com>
2023-01-18 20:12:03 +0100

    mongoDB conn

--- config/database.js
+++ config/database.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  try {
   mongoose.set("strictQuery", false);
-
-  mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  const db = mongoose.connection;
-  db.on("error", (error) => console.log(error));
-  db.once("open", () => console.log("Connected to Database"));
+  const db = await mongoose.connect(process.env.MONGO_URI);
+  console.log(`MongoDB Connected: ${db.connection.host}`);
+  
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
 };
 
 module.exports = connectDB;
