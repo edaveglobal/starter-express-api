d68b5beb749a67c736e7dd3cc7e45a694e99b68b
unknown <edaveglobal1@gmail.com>
2023-01-18 20:24:07 +0100

    connect str

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-  mongoose.set("strictQuery", false);
+  mongoose.set("useNewUrlParser", true);
   const db = await mongoose.connect(process.env.MONGO_URI);
   console.log(`MongoDB Connected: ${db.connection.host}`);
   
